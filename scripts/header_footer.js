0af9062d63aff8de6b890bf4fcb21ae6c697c5b7
Lfauzia <110790710+Lfauzia@users.noreply.github.com>
2024-02-21 22:06:28 -0500

    Update header_footer.js

---
+++ scripts/header_footer.js
@@ -0,0 +1,39 @@
+
+// Header content
+document.getElementById('header').innerHTML = `
+<header> 
+    <h1> Laura Fauzia || Legendary Fox || WEB250 </h1>
+    <!-- The navigation menu -->
+    <div class="navbar">
+        <a class="active" href="https://lfauzia.github.io/web250/index.html">Home</a>
+        <a href="https://lfauzia.github.io/web250/introduction.html">Introduction</a>
+        <a href="https://lfauzia.github.io/web250/contract.html">Contract</a>
+        <a href="https://lfauzia.github.io/web250/brand.html">Brand</a>
+    </div>
+</header>
+`;
+
+// Footer content
+document.getElementById('footer').innerHTML = `
+<footer>     
+    <div class="navbar-footer">  
+        <a href="https://github.com/Lfauzia">GitHub |</a> 
+        <a href="http://lfauzia.github.io/"> GitHub.io |</a>
+        <a href="https://lfauzia.github.io/web215/"> WEB215.io |</a>
+        <a href=" https://lfauzia.github.io/web250/"> WEB250.io |</a>
+        <a href="https://www.codecademy.com/profiles/lnkazan0"> Codecademy |</a>
+        <a href="https://www.freecodecamp.org/lnkazan0"> FreeCodeCamp |</a>
+        <a href="https://jsfiddle.net/user/Lnkazan0/fiddles/"> JSFiddle |</a>
+        <a href="https://www.linkedin.com/in/laura-f-07331a2aa/"> LinkedIn</a>
+        <p> &copy; 2024 Allé Youpi. All rights reserved.</p> 
+        <p> 
+            <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Flfauzia.github.io%2Fweb250%2Findex.html">
+            <img src="images/button_validation_html5.png" width="88" height="31" alt="Validate webpage HTML.">
+            </a>
+            <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Flfauzia.github.io%2Fweb250%2Fstyles%2Fstyles.css">
+            <img src="images/button_validation_css.png" width="88" height="31" alt="Validate webpage CSS.">
+            </a>
+        </p>         
+    </div>         
+</footer>
+`;
