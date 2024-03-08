c479a5c75d9674662a4e52844dfeb3c61c4d4949
Lfauzia <110790710+Lfauzia@users.noreply.github.com>
2024-03-07 20:38:57 -0500

    .

---
+++ components/introduction.php
@@ -0,0 +1,60 @@
+<h2>Introduction</h2>
+        <div><img src="images/2.png" alt="image 2">
+            <figure><img src="images/le-corbeau-et-le-renard.jpg" alt="Fox and Crow">
+                <figcaption >My favorite french poem:”Le Corbeau et le Renard”</figcaption>
+            </figure>
+            
+                <ul>
+                    <li>
+                    <strong>Personal Background:</strong> I grew up in the DRC and moved
+                    to Charlotte for some time now and since then, it has become my home.
+                    </li>
+                    <li>
+                    <strong>Professional Background:</strong> I have worked with Apple as
+                    a bilingual french technical support II for a few years before
+                    deciding to return to school.
+                    </li>
+                    <li>
+                    <strong>Academic Background:</strong> I started out in surgical
+                    technology but after a few semesters doing clinicals, (the smell of
+                    burning flesh, blood and the OR smell) I decided it wasn't for me, so
+                    I’m now completing my associates in IT- Full stack.
+                    </li>
+                    <li>
+                    <strong>Background in this Subject:</strong> I think it’s fascinating
+                    to code so I’m here to learn even more about it.
+                    </li>
+                    <li>
+                    <strong>Primary Computer Platform:</strong> macOS Sonoma (14.2.1)
+                    </li>
+                    <li>
+                    <strong>Courses I'm Taking & Why:</strong>
+                    <ul>
+                        <li>
+                        <strong><a href="https://lfauzia.github.io/web250/index.html">WEB250-N801</a> Database Driven Websites:</strong> Needed for
+                        my major
+                        </li>
+                        <li>
+                        <strong> <a href="https://lfauzia.github.io/web215/index.html">WEB215-N801 </a>Advanced Markup & Scripting:</strong> Needed for
+                        my major
+                        </li>
+                        <li>
+                        <strong>CSC221-N801 Advanced Python Programming:</strong> Needed
+                        for my major
+                        </li>
+                    </ul>
+                    </li>
+                    <li>
+                    <strong>Funny/Interesting Item to Remember me by:</strong> I really
+                    can’t stand public speech, it makes me panic to the point I lose my
+                    voice.
+                    </li>
+                    <li>
+                    <strong>I'd also like to Share:</strong> I like starting out with diy
+                    projects but I do not finish them.
+                    </li>
+                </ul>
+                <br>
+                
+
+        </div>
