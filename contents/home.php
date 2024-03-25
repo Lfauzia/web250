816adbd62d2314387f1512457ecb01c98c015dfc
Lfauzia <110790710+Lfauzia@users.noreply.github.com>
2024-03-25 16:37:55 -0400

    php update

--- contents/home.php
+++ contents/home.php
@@ -1,10 +1,10 @@
 <h2>Home</h2>
-    <div>
-        <img src="images/2.png" alt="image 2">
-        <p>Welcome to the Database Driven Websites class!</p>
-        <p>Prepare to embark on a journey of coding with me. We'll explore how to create amazing websites that interact
-            with databases, bringing your ideas to life!<br>
-            But before we dive into coding, let me introduce myself. I'm Laura, and if you are
-            a little curious about me? Dive into <a href="index.php?page=introduction">the Introduction Page</a> to
-            unravel the Fox's tale!</p>
-    </div>
+    
+    <img src="images/2.png" alt="image 2">
+    <p>Welcome to the Database Driven Websites class!</p>
+    <p>Prepare to embark on a journey of coding with me. We'll explore how to create amazing websites that interact
+    with databases, bringing your ideas to life!<br>
+    But before we dive into coding, let me introduce myself. I'm Laura, and if you are
+    a little curious about me? Dive into <a href="index.php?page=introduction">the Introduction Page</a> to
+    unravel the Fox's tale!</p>
+        
