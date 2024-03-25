4d3b14ae3066674505b1dbefb8532233179805ec
Lfauzia <110790710+Lfauzia@users.noreply.github.com>
2024-03-25 11:10:13 -0400

    links updates

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
 <body>
 <header>
     <h1>Laura Fauzia's Legendary Fox || WEB250</h1>
-    <div>
+    <nav>
         <a <?php if(isset($_GET['page']) && $_GET['page'] == 'home') echo 'class="active"'; ?> href="index.php?page=home">HOME</a>
         <a <?php if(isset($_GET['page']) && $_GET['page'] == 'introduction') echo 'class="active"'; ?> href="index.php?page=introduction">INTRODUCTION</a>
         <a <?php if(isset($_GET['page']) && $_GET['page'] == 'contract') echo 'class="active"'; ?> href="index.php?page=contract">CONTRACT</a>
         <a <?php if(isset($_GET['page']) && $_GET['page'] == 'brand') echo 'class="active"'; ?> href="index.php?page=brand">BRAND</a>
-    </div>
-</header>
+    </nav>
+</header><br>
 <main>
 <?php
     // Check if page parameter is set and not empty
@@ -38,31 +38,27 @@
         include 'contents/home.php';
     }
 ?>
-</main>
-<br>
-<br>
-<br>
-<footer>     
-        <div>                                    
-            <a href="https://github.com/Lfauzia">GitHub |</a> 
-            <a href="http://lfauzia.github.io/"> GitHub.io |</a>
-            <a href="https://lfauzia.github.io/web215/"> WEB215.io |</a>
-            <a href="https://www.codecademy.com/profiles/lnkazan0"> Codecademy |</a>
-            <a href="https://www.freecodecamp.org/lnkazan0"> FreeCodeCamp |</a>
-            <a href="https://jsfiddle.net/user/Lnkazan0/fiddles/"> JSFiddle |</a>
-            <a href="https://www.linkedin.com/in/laura-f-07331a2aa/"> LinkedIn</a>
+</main><br>
+
+<footer>                                                
+        <a href="https://github.com/Lfauzia">GitHub |</a> 
+        <a href="http://lfauzia.github.io/"> GitHub.io |</a>
+        <a href="https://lfauzia.github.io/web215/"> WEB215.io |</a>
+        <a href="https://www.codecademy.com/profiles/lnkazan0"> Codecademy |</a>
+        <a href="https://www.freecodecamp.org/lnkazan0"> FreeCodeCamp |</a>
+        <a href="https://jsfiddle.net/user/Lnkazan0/fiddles/"> JSFiddle |</a>
+        <a href="https://www.linkedin.com/in/laura-f-07331a2aa/"> LinkedIn</a>
                  
-            <p> &copy; 2024 Allé Youpi. All rights reserved.</p> 
+        <p> &copy; 2024 Allé Youpi. All rights reserved.</p> 
             
-            <p> 
-                <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Flfauzia.github.io%2Fweb250%2Findex.html">
-                <img src="images/button_validation_html5.png" width="88" height="31" alt="Validate webpage HTML.">
-                </a>
+        <p> 
+            <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Flfauzia.github.io%2Fweb250%2Findex.html">
+            <img src="images/button_validation_html5.png" width="88" height="31" alt="Validate webpage HTML.">
+            </a>
                      
-                <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2FLfauzia.github.io%2Fweb250%2Fstyles%2Fdefault.css">
-                <img src="images/button_validation_css.png" width="88" height="31" alt="Validate webpage CSS.">
-                </a>
-            </p>         
-        </div>         
+            <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2FLfauzia.github.io%2Fweb250%2Fstyles%2Fdefault.css">
+            <img src="images/button_validation_css.png" width="88" height="31" alt="Validate webpage CSS.">
+            </a>
+        </p>                          
     </footer>
 </body>    
