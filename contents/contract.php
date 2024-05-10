99dc65b9f0982e1b7945d49630b40f5c14f8d4f0
Lfauzia <110790710+Lfauzia@users.noreply.github.com>
2024-05-09 20:46:45 -0400

    Update contract.php

--- contents/contract.php
+++ contents/contract.php
@@ -1,11 +1,11 @@
 <h2>Course Contract</h2>
         
     <img src="images/2.png" alt="image 2"> 
-    <p>I, <a href="https://lfauzia.github.io/web250/introduction.html">Laura Fauzia</a>, agree to abide by the terms of the course contract in my Spring 2024, WEB215,Advanced Markup & Scripting with my instructor, Professor D.I. von Briesen.</p>
+    <p>I, <a href="https://lfauzia.github.io/web250/introduction.html">Laura </a>, agree to abide by the terms of the course contract in my Spring 2024, WEB215,Advanced Markup & Scripting with my instructor, Professor D.I. von Briesen.</p>
     <p>I understand that all work that I do on publicly available online tools will be available to the world, and will not put information there that is inappropriate for schoolwork, or that I wish to keep private.</p>
     <p>I also understand that it is my work that counts for attendance, not logins or showing up for class. As such, failure to turn in assignments may show as absences.</p>
     <p>I also understand that given the structure and content of this class it’s possible to find many examples online or even view my classmates’ code directly.</p>
     <p>I swear that I will only use these resources to learn, and will not cut and paste code except where I have properly given credit (i.e. external libraries) and never from my classmates.</p>
-    <p id="signature">Signed: Laura Fauzia , 20/01/2024 </p> 
+    <p id="signature">Signed: Laura, 20/01/2024 </p> 
               
        
